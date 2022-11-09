240439b14e5463ff3ebdadcc0e3693b770d9b111
William Silva <euwilliamsantana@gmail.com>
2022-11-09 09:40:00 -0300

    Estrutura visual - Componente: Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { ThemeProvider } from "styled-components"
+import { Transactions } from "./pages/Transactions"
 import { GlobalStyle } from "./styles/global"
 import { defaultTheme } from "./styles/themes/default"
 
@@ -6,9 +7,7 @@ export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyle/>
-      <div>
-        Hello World
-      </div>
+      <Transactions/>
     </ThemeProvider>
   )
 }
